dff25646388c6f05a026649d4b80209564b5602c
Michael Parker <mlparker@Michaels-MacBook-Pro-2.local>
2017-07-12 16:53:36 +0200

    Not sure what doing

--- ufo_functions.py
+++ ufo_functions.py
@@ -8,7 +8,7 @@ def net_rate(n_e,n_xi,n_xip1,n_xim1,alpha_rec, alpha_recm1, I_rat, I_ratm1):
 	# Time dependence of ionization balance (dn_Xi/dt)
 	# electron density, relative densities of ions (i, i+1, i-1), recombination coefficients, ionization rates
 
-	# Where to get these from? Cloudy?
+	# Where to get these from? Cloudy? Xstar? <- some sort of utility exists for taking rates etc from xstar, might be useful
 
 	recomb    = - n_xi * n_e * alpha_recm1 # recombination of Xi -> Xi-1
 	recomb_p1 = n_xip1 * n_e * alpha_rec   # recombination of Xi+1 -> Xi
