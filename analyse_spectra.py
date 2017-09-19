3740278c529bb28a476a31ad5f5113d072b32137
Michael Parker <parkermichael75@gmail.com>
2017-09-19 16:40:22 +0100

    minor changes and tidying

--- analyse_spectra.py
+++ analyse_spectra.py
@@ -10,19 +10,30 @@ from dcfirr import dcfirr
 
 colours=['dodgerblue','forestgreen','goldenrod','orangered','red'][::-1]
 ratio=True
+calc_dcf=False ### Douglas wrote the DCF script and it's crazy slow and I hate him.
 
 
 def get_binned_lightcurve(energies, eband, spectra_dset,times):
 	'''Returns the lightcurve (in stingray format) for a given energy band. Eband should be an iterable object with a lower and upper limit limit'''
 	### Find indices of matching energies. This is horribly inelegant.
-	# print eband[0],eband[1]
 	lowindex=np.where(energies>eband[0])[0][0]
 	highindex=np.where(energies<eband[1])[-1][-1]
-	# print lowindex, highindex
 	counts=np.sum(spectra_dset[:,lowindex:highindex+1],axis=1)
-	# pl.plot(counts)
 	return stingray.lightcurve.Lightcurve(times[1:counts.shape[0]+1],counts,input_counts=True)
 
+
+def make_covariance_spectrum(energies,spectra_dset,bins,times,reference_lc):
+	cov_spec=[]
+	for i in range(1,len(bins)):
+		temp_lc=get_binned_lightcurve(energies,(bins[i-1],bins[i]),spectra_dset,times)
+		cov=np.cov(temp_lc.counts,reference_lc.counts)
+		cov_spec.append(cov[0,1])
+	tempfig=pl.figure()
+	ax=pl.subplot(111)
+	ax.set_xscale()
+	pl.plot([(x+y)/2. for x,y in zip(bins[:-1],bins[1:])],cov_spec)
+	pl.show()
+	exit()
 
 
 
@@ -62,13 +73,13 @@ def main():
 	f2ax1.set_xlabel(r'$f\ (\mathrm{Hz})$')
 	f2ax1.set_ylabel('Lag (s)')
 
-	fig3=pl.figure()
-	f3ax1=pl.subplot(111)
-	f3ax1.set_xlim(-10000,10000)
-	f3ax1.set_xlabel('Lag (s)')
-	f3ax1.set_ylabel('Correlation')
-	# f3ax1.set_xscale('log')
-	# f3ax1.set_xlim(1)
+	if calc_dcf:
+		fig3=pl.figure()
+		f3ax1=pl.subplot(111)
+		f3ax1.set_xlim(-10000,10000)
+		f3ax1.set_xlabel('Lag (s)')
+		f3ax1.set_ylabel('Correlation')
+	
 
 	for i,density in enumerate(densities):
 
@@ -101,42 +112,40 @@ def main():
 		# ratio_lc.write('ratio_lc_%s' % str(i+1),format='ascii')
 		np.savetxt('ratio_lc_%s' % str(i+1),ratio_lc.counts)
 
-		segsize=20000
+		segsize=10000
+		print '\tCalculating time lag with',segsize,'s segments'
 		if ratio:
-			cross_spec=stingray.crossspectrum.AveragedCrossspectrum(ratio_lc,reference_lc,segment_size=20000)
+			cross_spec=stingray.crossspectrum.AveragedCrossspectrum(reference_lc,ratio_lc,segment_size=segsize)
 		else:
-			cross_spec=stingray.crossspectrum.AveragedCrossspectrum(test_lc,reference_lc,segment_size=20000)
+			cross_spec=stingray.crossspectrum.AveragedCrossspectrum(reference_lc,test_lc,segment_size=segsize)
 		# cross_spec.rebin_log(f=0.1)
 		timelag=cross_spec.time_lag()
-		f2ax1.errorbar(cross_spec.freq,timelag[0],timelag[1],lw=1,color=colours[i],label=density*1.e7)
+		f2ax1.plot(cross_spec.freq,timelag[0],lw=1,color=colours[i],label=density*1.e7)
+
+
+		if calc_dcf:
+			print '\tCalculating DCF using dcfirr.py'
+			print "\tThis is really slow, this is Douglas' fault. We hate Douglas."
+			if ratio:
+				lag,cor,numf,indices,indfinal=dcfirr(ratio_lc.time, ratio_lc.counts, reference_lc.time, reference_lc.counts, minpt=100000,minlag=-10000,maxlag=10000)
+			else:
+				lag,cor,numf,indices,indfinal=dcfirr(test_lc.time, test_lc.counts, reference_lc.time, reference_lc.counts, minpt=100000,minlag=-10000,maxlag=10000)
+			f3ax1.plot(lag,cor,lw=1,color=colours[i],label=density*1.e7)
+
+
+		#### Make covariance spectrum...
+		#make_covariance_spectrum(energies_dset[:],spectra_dset,energy_bins,times,reference_lc)
 
 
 
-
-		#### Plot reference lightcurve
-		# reference_lc.plot()
-		# pl.show()
-
-		#### Make image of spectra over time. TBC.
-		# pl.imshow(spectra.T,aspect=0.1)
-		# pl.show()
-		print '\nCalculating DCF using dcfirr.py'
-		print "This is really slow, this is Douglas' fault. We hate Douglas."
-		# if i==0:
-		if ratio:
-			lag,cor,numf,indices,indfinal=dcfirr(ratio_lc.time, ratio_lc.counts, reference_lc.time, reference_lc.counts, minpt=100000,minlag=-10000,maxlag=10000)
-		else:
-			lag,cor,numf,indices,indfinal=dcfirr(test_lc.time, test_lc.counts, reference_lc.time, reference_lc.counts, minpt=100000,minlag=-10000,maxlag=10000)
-		# else:
-			# lag,cor,numf,indices,indfinal=dcfirr(ratio_lc.time, ratio_lc.counts, reference_lc.time, reference_lc.counts, minpt=100000,minlag=-30000,maxlag=30000,indices=indices,indfinal=indfinal,numf=numf)
-		f3ax1.plot(lag,cor,lw=1,color=colours[i],label=density*1.e7)
-
-	pl.legend(loc='best', title=r'$\mathrm{Density}\ (\times10^7\ \mathrm{cm^{-3}})$', frameon=False)
+	if calc_dcf:
+		pl.legend(loc='best', title=r'$\mathrm{Density}\ (\times10^7\ \mathrm{cm^{-3}})$', frameon=False)
 
 	# pl.show()
 	fig1.savefig('analysed_lightcurves.pdf',bbox_inches='tight')
 	fig2.savefig('analysed_lagfreq.pdf',bbox_inches='tight')
-	fig3.savefig('analysed_dcf.pdf',bbox_inches='tight')
+	if calc_dcf:
+		fig3.savefig('analysed_dcf.pdf',bbox_inches='tight')
 
 
 	pass
